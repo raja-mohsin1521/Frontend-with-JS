feae7156e5f6808217b75197ba06885489a85200
raja-mohsin1521 <mohsinmunir150@gmail.com>
2024-03-05 01:57:46 +0500

    Updating Funtionalities handling events

--- Skin Analyizer Frontend/index.js
+++ Skin Analyizer Frontend/index.js
@@ -1,3 +1,5 @@
+let imagePreview2 = document.createElement('img');
+
 let analyizebtn=document.getElementById('analyizebtn')
 let comparebtn=document.getElementById('comparebtn')
 let analyizecard=document.getElementById('Analyize')
@@ -34,7 +36,7 @@ function drop() {
     document.querySelector('.dragBox').classList.remove('dragOver');
 }
 
-function handleFileUpload(input) {
+function handleFileUpload1(input) {
     const file = input.files[0];
 
     if (file) {
@@ -46,7 +48,7 @@ function handleFileUpload(input) {
             imagePreview.classList.add('imagePreview','img-fluid');
 
             const uploadBtn = document.getElementById('uploadbtn');
-            const dragBox = document.querySelector('.dragBox');
+            const dragBox = document.querySelector('.analizeimg');
 
             // Remove text and button
             dragBox.innerHTML = '';
@@ -61,3 +63,134 @@ function handleFileUpload(input) {
         reader.readAsDataURL(file);
     }
 }
+function dragNdrop2(event) {
+    fileName2 = URL.createObjectURL(event.target.files[0]);
+    
+    var previewImg = document.createElement("img");
+
+    previewImg.setAttribute("src", fileName2);
+    
+    let content2=document.getElementsByClassName('content2')[0]
+    content2.innerHTML=""
+    
+    let preview2=document.getElementsByClassName('showimg2')[0]
+    let containimg=preview2.querySelector('img');
+  if (containimg){
+    containimg.remove()
+    preview2.append(previewImg)
+
+  }
+  else{
+    preview2.append(previewImg)
+   
+  }
+    
+}
+function dragNdrop3(event) {
+    fileName3 = URL.createObjectURL(event.target.files[0]);
+   
+    var previewImg = document.createElement("img");
+    previewImg.setAttribute("src", fileName3);
+  
+  
+    let content3=document.getElementsByClassName('content3')[0]
+    content3.innerHTML=""
+    
+    let preview3=document.getElementsByClassName('showimg3')[0]
+    let containimg=preview3.querySelector('img');
+  if (containimg){
+    containimg.remove()
+    preview3.append(previewImg)
+
+  }
+  else{
+    preview3.append(previewImg)
+   
+  }
+    
+    
+}
+
+
+let scanbtn=document.getElementById('scanbtn')
+scanbtn.addEventListener("click",function(){
+
+if(comparebtn.classList.contains('active')){
+
+   
+    let fileInput1 = document.getElementById('uploadFile2');
+    let file1 = fileInput1.files[0];
+    
+    let formData1 = new FormData();
+    formData1.append('file', file1);
+   
+   
+    let fileInput2 = document.getElementById('uploadFile3');
+    let file2 = fileInput2.files[0];
+    
+    let formData2 = new FormData();
+    formData2.append('file', file2);
+
+
+console.log('Values inside formData1:');
+for (let pair of formData1.entries()) {
+    console.log(pair[0], pair[1]);
+}
+
+
+console.log('Values inside formData2:');
+for (let pair of formData2.entries()) {
+    console.log(pair[0], pair[1]);
+}
+
+let data = new FormData();
+data.append('src1', formData1);
+data.append('src2', formData2);
+
+
+console.log('Values inside FormData:');
+for (let pair of data.entries()) {
+    console.log(pair[0], pair[1]);
+}
+  
+let loader =document.getElementsByClassName('bg-text')[0]
+
+
+let p = fetch('http://0.0.0.0:8000/improvement',{
+            method: "POST",
+          
+    body:data})
+        p.then((x) => {
+
+            return x.json()
+
+        })
+.then(
+
+    
+    
+    response =>{ 
+       
+        
+        response.json()})
+  .then(data => {
+    
+    console.log(data);
+
+    const imageBeforeBase64 = data.image_before;
+    const imageAfterBase64 = data.image_after;
+
+    
+    console.log('data:image/png;base64,' + imageBeforeBase64) 
+    console.log('data:image/png;base64,' + imageAfterBase64) 
+  })
+  .catch(error => {
+    // Handle errors during the fetch
+    console.error('Error fetching data:', error);
+  });  
+
+
+}
+
+
+})
